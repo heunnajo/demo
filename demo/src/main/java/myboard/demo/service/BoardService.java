927ef9a6c5c71581c5921448d74a7626e725d775
heunnajo <61869540+heunnajo@users.noreply.github.com>
2024-07-08 21:55:43 +0900

    Update BoardService.java

--- demo/src/main/java/myboard/demo/service/BoardService.java
+++ demo/src/main/java/myboard/demo/service/BoardService.java
@@ -2,10 +2,10 @@ package myboard.demo.service;
 
 import lombok.RequiredArgsConstructor;
 import myboard.demo.domain.Board;
-import myboard.demo.repository.BoardRepository;
+import myboard.demo.dto.board.addBoardRequest;
+import myboard.demo.dto.board.updateBoardRequest;
 import myboard.demo.repository.jpaBoardRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,51 +13,42 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-    private final jpaBoardRepository boardRepository;
-
-    public Board save(Board board){
-        Board savedBoard = boardRepository.save(board);
-        return savedBoard;
+    private final jpaBoardRepository jpaBoardRepository;
+    private final myboard.demo.repository.jpaUserRepository jpaUserRepository;
+    public Board save(addBoardRequest request, String userId){
+        return jpaBoardRepository.save(request.toEntity(userId));
     }
-
-//    public Page<Board> findAll(Pageable pageable){
-//        return boardRepository.findAll(pageable);
-//    }
     public List<Board> findAll(){
-        return boardRepository.findAll();
+        return jpaBoardRepository.findAll();
     }
-
-    public Board findBoardById(Long id){
-        Board board = boardRepository.findById(id);//
+    public Board findById(long id){
+        return jpaBoardRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found: "+id));
+    }
+    //수정, 삭제는 해당 게시글을 작성한 사용자만 가능
+    public void delete(long id){
+        Board board = jpaBoardRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found: "+id));
+        authorizerArticleAuthor(board);
+        jpaBoardRepository.delete(board);
+    }
+    public Board update(long id, updateBoardRequest request){
+        Board board = jpaBoardRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("not found: "+id));
+        authorizerArticleAuthor(board);
+        board.update(board);
         return board;
-    }
-    public List<Board> findBoardByTitle(String title){
-        List<Board> boardList = boardRepository.findByTitle(title);
-        return boardList;
-    }
-
-    public List<Board> findBoardByContent(String content){
-        List<Board> boardList = boardRepository.findByTitle(content);
-        return boardList;
-    }
-
-    public List<Board> findBoardByWriter(String writer){
-        List<Board> boardList = boardRepository.findByTitle(writer);
-        return boardList;
-    }
-
-    public void deleteById(Long id){
-        boardRepository.deleteById(id);
     }
 
     //게시글을 작성한 유저인지 확인
-    private static void authorizeArticleAuthor(Board article) {
+    private static void authorizerArticleAuthor(Board board){
         String userName = SecurityContextHolder.getContext().getAuthentication().getName();
-        if(!article.getWriter().equals(userName)){
+        if(!board.getWriter().equals(userName)){
             throw new IllegalArgumentException("not authorized");
         }
     }
-    public Board saveComment(AddArticleRequest request,String userName){//AddArticleRequest 클래스에 저장된 값들을 article 데이터베이스에 저장한다.
+    public Board saveComment(addBoardRequest request, String userName){
         return jpaBoardRepository.save(request.toEntity(userName));
     }
+
 }
