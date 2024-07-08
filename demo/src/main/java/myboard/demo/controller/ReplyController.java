727338a62bcd8caf24d3a2f19832ab8537eee509
heunnajo <61869540+heunnajo@users.noreply.github.com>
2024-07-08 21:55:21 +0900

    Update ReplyController.java

--- demo/src/main/java/myboard/demo/controller/ReplyController.java
+++ demo/src/main/java/myboard/demo/controller/ReplyController.java
@@ -17,30 +17,4 @@ import java.util.List;
 @RequestMapping("/api/boards")
 public class ReplyController {
 
-    private final ReplyService replyService;
-
-    @PostMapping("/{boardId}/replies")
-    public ResponseEntity<?> save(@PathVariable Long id, @RequestBody Reply reply, Principal principal){
-        Reply savedReply = replyService.save(reply);
-        return new ResponseEntity<>(savedReply, HttpStatus.CREATED);
-    }
-    @GetMapping("/{boardId}/replies")
-    public ResponseEntity<?> getReplies(@PathVariable Long id){
-        List<Reply> list = replyService.findAll(id);
-        return ResponseEntity.ok(list);
-    }
-
-    @PutMapping("/{boardId}/replies/{replyId}")
-    public ResponseEntity<?> update(@PathVariable Long boardId, @PathVariable Long replyId, @RequestBody Reply updatedComment){
-        replyService.update(boardId, replyId, updatedComment);
-        return ResponseEntity.ok(replyId);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteBoard(@PathVariable("id") Long id){
-        boardService.deleteById(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-
 }
