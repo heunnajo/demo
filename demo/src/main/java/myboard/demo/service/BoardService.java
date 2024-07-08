0717f9f8e8857c8d7e1dc29405cf74efdbf2b075
조흔나 <gms950831@gmail.com>
2024-07-08 16:51:41 +0900

    Update BoardService.java

--- demo/src/main/java/myboard/demo/service/BoardService.java
+++ demo/src/main/java/myboard/demo/service/BoardService.java
@@ -3,6 +3,7 @@ package myboard.demo.service;
 import lombok.RequiredArgsConstructor;
 import myboard.demo.domain.Board;
 import myboard.demo.repository.BoardRepository;
+import myboard.demo.repository.jpaBoardRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-    private final BoardRepository boardRepository;
+    private final jpaBoardRepository boardRepository;
 
     public Board save(Board board){
         Board savedBoard = boardRepository.save(board);
@@ -27,7 +28,7 @@ public class BoardService {
     }
 
     public Board findBoardById(Long id){
-        Board board = boardRepository.findById(id);
+        Board board = boardRepository.findById(id);//
         return board;
     }
     public List<Board> findBoardByTitle(String title){
@@ -47,5 +48,16 @@ public class BoardService {
 
     public void deleteById(Long id){
         boardRepository.deleteById(id);
+    }
+
+    //게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Board article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if(!article.getWriter().equals(userName)){
+            throw new IllegalArgumentException("not authorized");
+        }
+    }
+    public Board saveComment(AddArticleRequest request,String userName){//AddArticleRequest 클래스에 저장된 값들을 article 데이터베이스에 저장한다.
+        return jpaBoardRepository.save(request.toEntity(userName));
     }
 }
