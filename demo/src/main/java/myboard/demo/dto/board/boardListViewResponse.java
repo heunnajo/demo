1a2307a93eda9f4bc70f07c419ef69f0e02a4327
heunnajo <61869540+heunnajo@users.noreply.github.com>
2024-07-08 21:55:27 +0900

    Create boardListViewResponse.java

---
+++ demo/src/main/java/myboard/demo/dto/board/boardListViewResponse.java
@@ -0,0 +1,19 @@
+package myboard.demo.dto.board;
+
+import lombok.Getter;
+import myboard.demo.domain.Board;
+
+@Getter
+public class boardListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    private final String author;
+    public ArticleListViewResponse(Board board){
+        this.id =board.getId();
+        this.title= board.getTitle();
+        this.content = board.getContent();
+        this.author =board.getWriter();
+    }
+}
