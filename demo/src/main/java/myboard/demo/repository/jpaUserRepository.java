0ab51450e820bc05c6bc575e4134e50f2dfd5490
조흔나 <gms950831@gmail.com>
2024-07-10 17:47:05 +0900

    Update jpaUserRepository.java

--- demo/src/main/java/myboard/demo/repository/jpaUserRepository.java
+++ demo/src/main/java/myboard/demo/repository/jpaUserRepository.java
@@ -2,5 +2,5 @@ package myboard.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface jpaUserRepository extends JpaRepository {
+public interface JpaUserRepository extends JpaRepository {
 }
