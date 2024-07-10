6351fed7b4e092d31dc26638c1f7a2217777391e
조흔나 <gms950831@gmail.com>
2024-07-10 17:47:18 +0900

    Create updateReplyRequest.java

---
+++ demo/src/main/java/myboard/demo/dto/reply/updateReplyRequest.java
@@ -0,0 +1,12 @@
+package myboard.demo.dto.reply;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class updateReplyRequest {
+    private String comment;
+}
