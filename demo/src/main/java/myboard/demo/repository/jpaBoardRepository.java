0deae2c12913a679a80c9acb2595d338593b5136
조흔나 <gms950831@gmail.com>
2024-07-10 17:47:15 +0900

    Update jpaBoardRepository.java

--- demo/src/main/java/myboard/demo/repository/jpaBoardRepository.java
+++ demo/src/main/java/myboard/demo/repository/jpaBoardRepository.java
@@ -3,6 +3,6 @@ package myboard.demo.repository;
 import myboard.demo.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface jpaBoardRepository extends JpaRepository<Board,Long> {
+public interface JpaBoardRepository extends JpaRepository<Board,Long> {
 
 }
