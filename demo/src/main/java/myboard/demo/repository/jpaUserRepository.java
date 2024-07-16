556aaac78f0d9bd39437b9ed831994037801fc37
조흔나 <gms950831@gmail.com>
2024-07-16 22:46:18 +0900

    Update jpaUserRepository.java

--- demo/src/main/java/myboard/demo/repository/jpaUserRepository.java
+++ demo/src/main/java/myboard/demo/repository/jpaUserRepository.java
@@ -1,6 +1,11 @@
 package myboard.demo.repository;
 
+import myboard.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaUserRepository extends JpaRepository {
+    Optional<User> findByEmail(String email);
+    User findByMemberId(String memberId);
 }
