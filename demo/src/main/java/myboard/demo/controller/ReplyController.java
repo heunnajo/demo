8f20041fe385b22ce26d2c751de822168a680066
조흔나 <gms950831@gmail.com>
2024-07-16 22:46:27 +0900

    Update ReplyController.java

--- demo/src/main/java/myboard/demo/controller/ReplyController.java
+++ demo/src/main/java/myboard/demo/controller/ReplyController.java
@@ -3,6 +3,8 @@ package myboard.demo.controller;
 import lombok.RequiredArgsConstructor;
 import myboard.demo.domain.Board;
 import myboard.demo.domain.Reply;
+import myboard.demo.dto.reply.addReplyRequest;
+import myboard.demo.dto.reply.updateReplyRequest;
 import myboard.demo.service.BoardService;
 import myboard.demo.service.ReplyService;
 import org.springframework.http.HttpStatus;
@@ -14,7 +16,33 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/boards")
+@RequestMapping("/api")
 public class ReplyController {
+    private final ReplyService replyService;
 
+    //댓글 생성
+    @PostMapping("/boards/{id}/replies")
+    public ResponseEntity<Reply> save(@PathVariable Long replyId, @RequestBody addReplyRequest request, Principal principal){
+        Reply savedReply = replyService.save(replyId,request,principal.getName());
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedReply);
+    }
+
+    //댓글 읽어오기
+    @GetMapping("/boards/{id}/replies")
+    public List<Reply> read(@PathVariable long id){
+        return replyService.findAll(id);
+    }
+
+    //댓글 업데이트
+    @PutMapping("/boards/{boardId}/replies/{id}")
+    public ResponseEntity<Long> update(@PathVariable long boardId, @PathVariable Long id, @RequestBody updateReplyRequest dto){
+        replyService.update(boardId,id,dto);
+        return ResponseEntity.ok(id);
+    }
+    //댓글 삭제
+    @DeleteMapping("/boards/{boardId}/replies/{id}")
+    public ResponseEntity<Long> delete(@PathVariable long boardId, @PathVariable Long id){
+        replyService.delete(boardId,id);
+        return ResponseEntity.ok(id);
+    }
 }
