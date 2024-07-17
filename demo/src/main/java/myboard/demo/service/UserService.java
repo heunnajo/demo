f7cf2aea98a152a9fd228ab8f21bcca145043fa0
heunnajo <61869540+heunnajo@users.noreply.github.com>
2024-07-17 20:55:10 +0900

    Update UserService.java

--- demo/src/main/java/myboard/demo/service/UserService.java
+++ demo/src/main/java/myboard/demo/service/UserService.java
@@ -16,12 +16,12 @@ public class UserService {
     public Long save(addUserRequest dto){
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        return jpaUserRepository.save(User.builder())
-                .email(dto.getEmail())
-                .password(encoder.encode(dto.getPassword()))
-                .memberId(dto.getEmail())
-                .build().getId();
-
+        User savedUser = jpaUserRepository.save(User.builder()
+                        .email(dto.getEmail())
+                        .pw(encoder.encode(dto.getPassword()))
+                        .memberId(dto.getMemberId())
+                        .build());
+        return savedUser.getId();
     }
 
     public User findById(String memberId){
