d81e6de350032affbb356acdd6c8a4ab0941a261
heunnajo <61869540+heunnajo@users.noreply.github.com>
2024-07-17 20:57:45 +0900

    Update jpaBoardRepository.java

--- demo/src/main/java/myboard/demo/repository/jpaBoardRepository.java
+++ demo/src/main/java/myboard/demo/repository/jpaBoardRepository.java
@@ -1,8 +1,5 @@
 package myboard.demo.repository;
-
 import myboard.demo.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface JpaBoardRepository extends JpaRepository<Board,Long> {
-
 }
