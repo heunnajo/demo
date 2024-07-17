be9ae57d085116d9b5f50bd57b90b86050bb5813
heunnajo <61869540+heunnajo@users.noreply.github.com>
2024-07-17 20:57:43 +0900

    Update jpaUserRepository.java

--- demo/src/main/java/myboard/demo/repository/jpaUserRepository.java
+++ demo/src/main/java/myboard/demo/repository/jpaUserRepository.java
@@ -1,11 +1,8 @@
 package myboard.demo.repository;
-
 import myboard.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
-
-public interface JpaUserRepository extends JpaRepository {
+public interface JpaUserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
-    User findByMemberId(String memberId);
+    Optional<User> findByMemberId(String memberId);
 }
